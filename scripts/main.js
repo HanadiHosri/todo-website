20b190955a60299296b6d97db4e540e36b9f46f4
unknown <hanadi.hosri@gmail.com>
2024-03-01 09:27:52 +0200

    adding remove task and complete task

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,38 @@ function addTask() {
     if (inputValue == "") {
         alert("please write a task!");
     } else {
+        li.appendChild(createCompleteButton(li));
+        li.appendChild(createCloseButton(li));
         document.getElementById("list").appendChild(li);
     }
 
     taskInput.value = "";
-    
+}
+
+function createCloseButton(task) {
+    let closeButton = document.createElement("button");
+    closeButton.appendChild(document.createTextNode("\u00D7"));
+    closeButton.onclick = function () {
+        removeTask(task);
+    }
+    return closeButton;
+}
+
+function removeTask(task) {
+    let div = task;
+    div.style.display = "none";
+}
+
+function createCompleteButton(task) {
+    let completeButton = document.createElement("button");
+    completeButton.appendChild(document.createTextNode("\u2713"));
+    completeButton.onclick = function () {
+        completeTask(task);
+    }
+    return completeButton;
+}
+
+function completeTask(task) {
+    let div = task;
+    div.style.textDecoration = "line-through";
 }
